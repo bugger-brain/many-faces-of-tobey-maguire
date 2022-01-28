dd1a8c13fa3043f35cd590e47e5623a37cc02199
Paul <pgotcher@dev-10.com>
2022-01-28 14:57:32 -0600

    controller and http requests added

--- server/src/main/java/maguire/controllers/MaguireController.java
+++ server/src/main/java/maguire/controllers/MaguireController.java
@@ -1,6 +1,35 @@
 package maguire.controllers;
 
+import maguire.domain.MaguireService;
+import maguire.domain.TobeyTypeService;
+import maguire.models.Maguire;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/maguire")
 public class MaguireController {
+
+    private final MaguireService maguireService;
+    private final TobeyTypeService tobeyTypeService;
+
+    public MaguireController(MaguireService maguireService, TobeyTypeService tobeyTypeService) {
+        this.maguireService = maguireService;
+        this.tobeyTypeService = tobeyTypeService;
+    }
+
+    @GetMapping
+    public List<Maguire> getMaguires() {
+        return maguireService.findAll();
+    }
+
+//    @GetMapping
+//    public
 
 }
 

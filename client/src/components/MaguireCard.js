28b3989036fd2869bce000d4fb6026ce8ba8c97c
Stephanie <stephanie_sylva@uri.edu>
2022-02-01 10:04:24 -0600

    Updated NavBar

--- client/src/components/MaguireCard.js
+++ client/src/components/MaguireCard.js
@@ -11,7 +11,7 @@ function MaguireCard({ maguire }) {
             <div className="card-body">
                 <h5 className="card-title">{maguire.name}</h5>
                 <h6 className="card-subtitle">{maguire.description}</h6> 
-                <h7 className="card-text">{maguire.tobeyType}</h7>
+                <h7 className="card-text">{maguire.tobeyTypes}</h7>
             </div>
         </div>
         //could add link to edit or delete here but idk for a fanpage

84faa4647e1fc796326c7c923dc5770c4f6e0adc
Paul <pgotcher@dev-10.com>
2022-02-02 11:17:58 -0600

    whoops! changes added back

---
+++ client/src/components/Bio.js
@@ -0,0 +1,43 @@
+function Bio(){
+    return (
+        <main>
+            <h1>All About Tobey</h1>
+        <div class='container'>
+            <div class="col-lg-6">
+                <fieldset class="border" >
+                    <legend class ='text-center'>Tobey Fast Facts:</legend>
+                        <p>
+                        Full Name: Tobey Vincent Maguire <br />
+                        Birthday: June 27, 1975 <br />
+                        Age: 46 <br />
+                        Zodiac Sign: Cancer <br />
+                        Spouse(Ex): Jennifer Meyer <br />
+                        Children: Otis Tobias Maguire, Ruby Sweetheart Maguire
+    
+                    </p>
+                </fieldset>
+            </div>
+        </div>
+        <br />
+        <div class='container'>
+            <fieldset class="border">
+            <legend class ="text-center">Biography(from short-biography.com)</legend>
+            <p>
+            Tobey Maguire is and American producer and actor. He is best known for his lead role in the Spider-Man Trilogy films. He also worked in some other good movies like Seabiscuit (2003), The Good German (2006), Brothers (2009), and The Great Gatsby (2013) etc. <br /> <br/> 
+    
+    Tobey Maguire’s full name is Tobias Vincent “Tobey” Maguire. Tobey born on June 27, 1975, in Santa Monica, California. He is the son of Wendy Maguire and Vincent Maguire. His mother is also a Screenwriter and film producer. Tobey’s father is a construction worker and cook. He has four half-brothers. He started his drama class when he was in sixth grade. After that, he dropped out from his high school and didn’t continue further. Then in 2000, he obtained his GED. <br /> <br />
+    
+    Tobey Maguire’s first film was The Wizard(1989). He had no lines in that film. After that, he appeared in various commercials and TV series and movies as supporting cast. During the auditions, he met another rising Hollywood star Leonardo DiCaprio. They quickly became friends. They did some work together back then. After that, he got his break in one of the films of Ang Lee named The Ice Storm(1997). Then he got a chance to play the lead role in some other movies like Pleasantville(1998), The Cider House Rules(1999), and Wonder Boys(2000) etc.<br /> <br />
+    Moreover, in 2002, Tobey signed for the big-budget movie Spider-Man(2002). It was based on the famous Marvel Comics superhero series. After its big success, he also featured in the sequels of this movie Spider-Man2(2004) and Spider-Man3(2007). Later in 2008, he appeared with Robert Downey Jr. in the movie Tropic Thunder. Then in 2009, he starred with Natalie Portman and Jake Gyllenhaal in the movie Brothers. After that, he played the role of the narrator with Leonardo DiCaprio in the movie The Great Gatsby(2013). <br/> <br />
+    Tobey Maguire and his Spider-Man co-star Kirsten Dunst dated for a while in 2001. After that, he met Jennifer Meyer in 2003. They engaged in 2006 and married on September 3, 2007. They had a daughter in 2006 named Ruby Sweetheart Maguire. Tobey and Kristen also had another son named Tobias Maguire in May 2009. But after nine years they announced their separation on October 18, 2016.
+    Tobey is also a professional poker player. <br />
+            </p>
+            </fieldset>
+        </div>
+    
+    
+    </main>
+    );
+    }
+    
+    export default Bio;

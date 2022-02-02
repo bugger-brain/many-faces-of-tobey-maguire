8c38eadf37c6ee6633b012b9daa58416286abb20
Stephanie <stephanie_sylva@uri.edu>
2022-02-02 10:58:58 -0600

    Message form updated

--- client/src/components/MaguireForm.js
+++ client/src/components/MaguireForm.js
@@ -8,7 +8,7 @@ function MaguireForm() {
         name: "",
         description: "",
         imageUrl: "",
-        tobeyTypes: []
+        powers: []
     });
     const [tobeyTypes, setTobeyTypes] = useState([]);
     const [errors, setErrors] = useState([]);
@@ -69,98 +69,6 @@ function MaguireForm() {
             add();
         }
     }
-
-    function add() {
-
-        const init = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(maguire)
-        };
-
-        fetch("http://localhost:8080/maguire", init)
-            .then(response => {
-                if (response.status === 201) {
-                    history.push("/");
-                } else {
-                    return response.json();
-                }
-            }).then(result => {
-                if (result) {
-                    setErrors(result.messages);
-                }
-            }).catch(console.error);
-    }
-
-    function update() {
-        const init = {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(maguire)
-        };
-        fetch(`http://localhost:8080/maguire/${maguire.maguireId}`, init)
-            .then(response => {
-                if (response.status === 204) {
-                    history.push("/");
-                } else if (response.status === 404) {
-                    setErrors(["Maguire not found."]);
-                } else {
-                    return response.json();
-                }
-            }).then(result => {
-                if (result) {
-                    setErrors(result.messages);
-                }
-            }).catch(console.log);
-    }
-
-    return (
-        <>
-            {errors.length > 0 && 
-            <div className="alert alert-danger">
-                <ul>
-                    {errors.map(err => <li>{err}</li>)}
-                </ul>
-            </div>}
-            <form onSubmit={onSubmit}>
-                <h2>{maguire.maguireId ? "Edit" : "Add"} Maguire</h2>
-                <div className="mb-2">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input id="name" name="name" className="form-control"
-                        onChange={onChange} value={maguire.name}></input>
-                </div>
-                <div className="mb-2">
-                    <label htmlFor="description" className="form-label">Description</label>
-                    <input id="description" name="description" className="form-control"
-                        onChange={onChange} value={maguire.description}></input>
-                </div>
-                <div className="mb-2">
-                    <label htmlFor="imageUrl" className="form-label">Image URL</label>
-                    <input id="imageUrl" name="imageUrl" type="url" className="form-control"
-                        onChange={onChange} value={maguire.imageUrl}></input>
-                </div>
-                {/* <div className="mb-2">
-                    <h3>TobeyTypes</h3>
-                    {powers.map(p => <div key={p.powerId} className="form-check">
-                        <input className="form-check-input" type="checkbox"
-                            value={p.powerId} checked={maguire.powers.find(i => i.powerId === p.powerId)} onChange={onChange}
-                            id={"power" + p.powerId} name="powers"></input>
-                        <label className="form-check-label" htmlFor={"power" + p.powerId}>
-                            {p.name}
-                        </label>
-                    </div>)}
-                </div> */}
-                <div>
-                    <button type="submit" className="btn btn-primary me-2">Save</button>
-                    <Link to="/" className="btn btn-info">Cancel</Link>
-                </div>
-            </form >
-        </>
-    );
 
 }
 
--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import {Link, useHistory} from "react-router-dom";
-function MessageForm(){
-    const defaultMessage=[
+import { Link, useHistory } from "react-router-dom";
+
+function MessageForm() {
+    const defaultMessage = [
         {
-        name: "",
-        description: ""
+            name: "",
+            description: ""
         }
     ];
 
@@ -14,14 +15,12 @@ function MessageForm(){
 
     const history = useHistory();
 
-   
-
     const onSubmit = (event) => {
         event.preventDefault();
 
-        const updatedMessages = {...messages};
+        const updatedMessages = { ...messages };
 
-        const newMessage={
+        const newMessage = {
             name,
             description
         };
@@ -30,41 +29,45 @@ function MessageForm(){
 
         setName("");
         setDescription("");
-        
+
     }
-    return(
+    return (
         <form onSubmit={onSubmit}>
-        <h2>Leave a Message for Tobey</h2>
+            <h2>Leave a Message for Tobey</h2>
 
-        <div>
-            <label htmlFor="name"> Name: </label>
-            <input
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-             onChange ={event=> setName(event.target.value)}
-            />
-           
-        </div>
-        <div>
-            <label htmlFor="description"> Your Message: </label>
-            <input
-            type="text"
-            id="description"
-            name="description"
-            value={description}
-            onChange={event=> setDescription(event.target.value)}
-            />
-        </div>
+            <div>
+                <label htmlFor="name"> Name: </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                />
 
-        <div>
-            <button type="submit">Send</button>
-            <Link to="/"> Cancel</Link>
-        </div>
+            </div>
+            <div>
+                <label htmlFor="description"> Your Message: </label>
+                <textarea class="form-control"
+                    type="text"
+                    id="description"
+                    name="description"
+                    value={description}
+                    onChange={event => setDescription(event.target.value)}
+                    rows="3">
+                </textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="formFileMultiple" class="form-label">Attach an image!</label>
+                <input class="form-control" type="file" id="formFileMultiple" multiple></input>
+            </div>
+
+            <div>
+                <button type="submit">Send</button>
+                <button> <Link to="/"> Cancel</Link></button>
+            </div>
         </form>
-
-
     );
 }
 

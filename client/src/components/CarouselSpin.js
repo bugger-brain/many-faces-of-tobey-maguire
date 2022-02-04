ed719777fcf07580c77348784c6522d986082f4e
Stephanie <stephanie_sylva@uri.edu>
2022-02-04 11:38:42 -0600

    Tobey of the day working

--- client/src/components/CarouselSpin.js
+++ client/src/components/CarouselSpin.js
@@ -1,17 +1,58 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import TobeyCard from "./TobeyCard";
 import Tobey from "./Tobey";
 
 function CarouselSpin() {
 
+    const [tobeys, setTobeys] = useState([]);
+    const [currentTobey, setCurrentTobey] = useState(1);
+
+    useEffect(() => {
+        randomize();
+        fetch(`http://localhost:8080/api/tobey`)
+            .then(response => {
+                if (response.status ===200) {
+                    return response.json();
+                }
+                return Promise.reject("bad fetch");
+            }).then(tobeys => setTobeys(tobeys))
+            .catch(console.error);
+    }, []);
+
+    function randomize() {
+        const idMap = tobeys.map(t => t.tobeyId);
+        const randomIndex = Math.floor(Math.random() * idMap.length);
+        
+        setCurrentTobey(idMap[randomIndex]);
+    }
+
     return (
         <div>
 
-            <center><button type="button" class="button">
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+        
+                       {tobeys.map(tobey => {
+                       return <div key={tobey.tobeyId} className = {`col carousel-item ${tobey.tobeyId === currentTobey && "active" }`}>
+                       <TobeyCard tobey ={tobey} />
+               </div>})}
+                   
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <center><button type="button" onClick={randomize} class="button btn-lg btn-warning">
                 <span class="button__text">Spin for your Tobey of the Day!</span>
             </button></center>
-            
+
         </div>
 
 
